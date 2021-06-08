609e165432499dcfe5098b3e55ec7d2f7a0ee721
Louis PLAQUET <louis.plaquet@gmail.com>
2021-06-08 11:40:24 +0200

    Delete all js files useless

--- javascript/NavBar.js
+++
@@ -1,9 +0,0 @@
-//using AJAX to get HTML component and put it into index.html
-let requestNavbar = new XMLHttpRequest();
-requestNavbar.open("GET", "HTML/NavBar/NavBar.html", true);
-requestNavbar.send();
-requestNavbar.onreadystatechange = () => { 
-    if (requestNavbar.readyState === 4 && requestNavbar.status === 200) {
-        document.getElementsByClassName("header")[0].insertAdjacentHTML('afterend', requestNavbar.responseText);
-    }
-};
--- javascript/Presentation.js
+++ javascript/Presentation.js
@@ -1,14 +1,1 @@
-//using AJAX to get HTML component and put it into index.html
-/*
-let requestPresentation = new XMLHttpRequest();
-requestPresentation.open("GET", "HTML/Presentation/presentation.html", true);
-requestPresentation.send();
-requestPresentation.onreadystatechange = () => { 
-    if (requestPresentation.readyState === 4 && requestPresentation.status === 200) {
-        document.getElementsByClassName("navBar")[0].insertAdjacentHTML('afterend', requestPresentation.responseText);
-        //============================================================
-        scrollAnimation();
-    }
-};
-*/
 scrollAnimation();
