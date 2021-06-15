470ea9ba7e733b4347b0b4bc081c61bd633fa5fb
Louis PLAQUET <louis.plaquet@gmail.com>
2021-06-15 16:09:14 +0200

    Delete useless js files

--- javascript/FirstContainer.js
+++
@@ -1,9 +0,0 @@
-//============================================================
-let backBtn = document.getElementsByClassName("btnBack");
-backBtn[0].setAttribute("disabled", "true");
-backBtn[0].style.opacity = ".3";
-backBtn[0].style.cursor = "auto";
-//============================================================
-checkFlickerBtns();
-//============================================================
-clickOnForwardAndBackBtns();
